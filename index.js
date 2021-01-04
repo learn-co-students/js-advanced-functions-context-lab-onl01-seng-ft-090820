06d9b36ef0cd3b0edd7adbbe4ac34cebda7e7348
palepanorama <matthew.keith.thomas@gmail.com>
2021-01-04 04:25:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-/* Your Code Here */
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date
+    })
+    return this;
+}
+
+function createTimeOutEvent(timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date 
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeOut = this.timeOutEvents.find(i => i.date === date).hour
+    let timeIn = this.timeInEvents.find(i => i.date === date).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+    let wages = hoursWorkedOnDate.call(this, date) * this.payPerHour 
+    return wages;
+}
+
+function calculatePayroll(array) {
+    return array.reduce(function(memo, rec) {
+        return memo + allWagesFor.call(rec)}, 0)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(i => i.firstName === firstName)
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
